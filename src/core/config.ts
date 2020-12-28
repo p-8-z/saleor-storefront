eda8f70a79f2bc06b20c3b89c26cc7227a15cb2c
p8z <pav.zabka@gmail.com>
2020-12-28 21:48:43 +0100

    locale meta css

--- src/core/config.ts
+++ src/core/config.ts
@@ -64,9 +64,9 @@ export const SOCIAL_MEDIA = [
 export const META_DEFAULTS = {
   custom: [],
   description:
-    "Open-source PWA storefront built with Saleor's e-commerce GraphQL API. Written with React and TypeScript.",
+    "Quadrus PWA storefront built with Saleor's e-commerce GraphQL API. Written with React and TypeScript.",
   image: `${window.location.origin}${require("../images/logo.svg")}`,
-  title: "Demo PWA Storefront – Saleor Commerce",
+  title: "Quadrus Commerce",
   type: "website",
   url: window.location.origin,
 };
